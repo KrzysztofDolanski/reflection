452b8b190cc26109d7af6b8db8bf48ce4af83a9d
Krzysztof Dolanski <krzysztof_dolanski@epam.com>
2022-05-06 16:45:31 +0200

    annotation second test

--- src/main/java/com/epam/dolanski/create_annotation/Anno.java
+++ src/main/java/com/epam/dolanski/create_annotation/Anno.java
@@ -3,6 +3,7 @@ package com.epam.dolanski.create_annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Anno {
 
     int value() default 10;
--- src/test/java/com/epam/dolanski/SampleClassTest.java
+++ src/test/java/com/epam/dolanski/SampleClassTest.java
@@ -4,16 +4,29 @@ import com.epam.dolanski.create_annotation.Anno;
 import com.epam.dolanski.create_annotation.SampleClass;
 import org.testng.annotations.Test;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class SampleClassTest {
 
+    SampleClass sampleClass = new SampleClass();
 
     @Test
-    void annotationShouldHaveProperName() throws NoSuchMethodException {
-        SampleClass sampleClass = new SampleClass();
+    void annotationShouldHaveNameAnno() {
+        // Given
+        Method[] declaredMethods = sampleClass.getClass().getDeclaredMethods();
+        // When
+        Annotation annotation = Arrays.stream(Arrays.stream(declaredMethods).findFirst().orElseThrow().getDeclaredAnnotations()).findFirst().orElseThrow();
+        // Then
+        assertTrue(annotation.toString().contains("Anno"));
+    }
+
+    @Test
+    void annotationShouldHaveValueEquals10() throws NoSuchMethodException {
         // Given
         Method method = sampleClass.getClass().getMethod("sampleMethod");
         // When
